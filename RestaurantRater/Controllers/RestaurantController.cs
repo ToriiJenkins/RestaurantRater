1c6306fe1cb7598fa9992e9170e8a2bc81321234
tjenkins <torii.j.jenkins@gmail.com>
2020-11-28 20:44:07 -0500

    Commit with Setup, Model Setup,Controller setup, Index view, Create and Delete functionality

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantRater.Models;
+using static RestaurantRater.Models.Restaurant;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            return View (_db.Restaraunts.ToList());
+        }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        //POST: RestaurantCreate
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaraunts.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        //GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id ==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaraunts.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaraunts.Find(id);
+            _db.Restaraunts.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+
+    
+}
